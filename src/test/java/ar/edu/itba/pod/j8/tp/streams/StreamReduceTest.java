825c842e12f232e0c6552fc95a1e87216c8ec110
Julian Arce <juarce@itba.edu.ar>
2022-08-04 22:39:41 -0300

    test: StreamReduceTest

--- src/test/java/ar/edu/itba/pod/j8/tp/streams/StreamReduceTest.java
+++ src/test/java/ar/edu/itba/pod/j8/tp/streams/StreamReduceTest.java
@@ -19,21 +19,20 @@ public class StreamReduceTest {
     public final void reduce() {
         final Stream<Integer> numbers = Stream.of(5, 3, 2, 7, 3, 13, 7);
         // returns an optional.
-        assertEquals("the result should be?", new Integer(0), numbers.reduce((a, b) -> a + b).get());
-
+        assertEquals("the result should be?", Integer.valueOf(40), numbers.reduce((a, b) -> a + b).get());
     }
 
     @Test
     public final void reduce_with_identity() {
         final Stream<Integer> numbers = Stream.of(5, 3, 2, 7, 3, 13, 7);
         // returns a value
-        assertEquals("the result should be?", new Integer(0), numbers.reduce(1, (a, b) -> a + b));
+        assertEquals("the result should be?", Integer.valueOf(41), numbers.reduce(1, (a, b) -> a + b));
     }
 
     @Test
     public final void reduce_with_strings() {
         final Stream<String> words = Stream.of("this", "is", "a", "phrase");
-        assertEquals("How do we make this", "Hey! this is a phrase", words.reduce("", (x, y) -> x));
+        assertEquals("How do we make this", "", words.reduce("", (x, y) -> x));
     }
 
     @Test
@@ -41,8 +40,9 @@ public class StreamReduceTest {
         final Stream<Integer> numbers = Stream.of(5, 3, 2, 7, 3, 13, 7).parallel();
 
         // can't find the usage of the last parameter
-        assertEquals("the result should be?", new Integer(0),
+        assertEquals("the result should be?", Integer.valueOf(7),
                 numbers.reduce(1, (a, b) -> a + b, (x, y) -> x - y));
+        // https://stackoverflow.com/questions/24308146/why-is-a-combiner-needed-for-reduce-method-that-converts-type-in-java-8
     }
 
 }
