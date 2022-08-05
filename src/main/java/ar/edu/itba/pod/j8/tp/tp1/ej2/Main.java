f673609a6eb49191fab30a9affcea1541890d604
Julian Arce <juarce@itba.edu.ar>
2022-08-04 23:58:08 -0300

    feat: tp1 ej2.1

---
+++ src/main/java/ar/edu/itba/pod/j8/tp/tp1/ej2/Main.java
@@ -0,0 +1,83 @@
+package ar.edu.itba.pod.j8.tp.tp1.ej2;
+
+import ar.edu.itba.pod.j8.tp.tp1.ej2.model.Continent;
+import ar.edu.itba.pod.j8.tp.tp1.ej2.model.Country;
+
+import java.time.LocalDate;
+import java.util.Arrays;
+import java.util.Collection;
+
+public class Main {
+
+    public static void main(String[] args) {
+        Collection<Country> countries = Arrays.asList(
+                new Country("Argentina", Continent.AMERICA, 43590400, LocalDate.parse("1816-07-09")),
+                new Country("China", Continent.ASIA, 1377939144, null),
+                new Country("Brazil", Continent.AMERICA, 206284591, LocalDate.parse("1822-09-07")),
+                new Country("Nigeria", Continent.AFRICA, 189636000, LocalDate.parse("1960-10-01")),
+                new Country("Japón", Continent.ASIA, 126675000, null),
+                new Country("México", Continent.AMERICA, 122273000, LocalDate.parse("1810-09-16")),
+                new Country("Turquía", Continent.ASIA, 79265000, LocalDate.parse("1923-10-29")),
+                new Country("Australia", Continent.OCEANIA, 24117000, null),
+                new Country("Chile", Continent.AMERICA, 18192000, LocalDate.parse("1810-09-18")),
+                new Country("Bélgica", Continent.EUROPE, 11338000, LocalDate.parse("1930-10-04")),
+                new Country("Portugal", Continent.EUROPE, 10262000, LocalDate.parse("1640-12-01")),
+                new Country("Bolivia", Continent.AMERICA, 10985000, LocalDate.parse("1825-08-06")),
+                new Country("Suecia", Continent.EUROPE, 9824000, LocalDate.parse("1523-06-06")),
+                new Country("Israel", Continent.ASIA, 8391000, LocalDate.parse("1948-05-15")),
+                new Country("Inglaterra", Continent.EUROPE, 53010000, null),
+                new Country("Serbia", Continent.EUROPE, 7071000, LocalDate.parse("1804-02-15"))
+        );
+
+        /**
+         * 1. Imprimir en pantalla todos los países
+         */
+        countries.stream().forEach(System.out::println);
+
+        /**
+         * 2. Imprimir en pantalla todos los países
+         */
+
+        /**
+         * 3. Imprimir en pantalla todos los países
+         */
+
+        /**
+         * 4. Imprimir en pantalla todos los países
+         */
+
+        /**
+         * 5. Imprimir en pantalla todos los países
+         */
+
+        /**
+         * 6. Imprimir en pantalla todos los países
+         */
+
+        /**
+         * 7. Imprimir en pantalla todos los países
+         */
+
+        /**
+         * 8. Imprimir en pantalla todos los países
+         */
+
+        /**
+         * 9. Imprimir en pantalla todos los países
+         */
+
+        /**
+         * 10. Imprimir en pantalla todos los países
+         */
+
+        /**
+         * 11. Imprimir en pantalla todos los países
+         */
+
+        /**
+         * 12. Imprimir en pantalla todos los países
+         */
+
+    }
+
+}
