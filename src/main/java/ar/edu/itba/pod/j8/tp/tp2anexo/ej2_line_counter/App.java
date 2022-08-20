4d4b6d8d6ca12a1aba76c7ae87771c01f9c519ee
Julian Arce <juarce@itba.edu.ar>
2022-08-20 13:34:58 -0300

    feat: tp2 anexo ej2

--- src/main/java/ar/edu/itba/pod/j8/tp/tp2anexo/ej2_line_counter/App.java
+++ src/main/java/ar/edu/itba/pod/j8/tp/tp2anexo/ej2_line_counter/App.java
@@ -1,4 +1,14 @@
 package ar.edu.itba.pod.j8.tp.tp2anexo.ej2_line_counter;
 
+import java.util.concurrent.ExecutionException;
+
 public class App {
+    public static void main(String[] args) {
+        String dir = "src/main/java/ar/edu/itba/pod/j8/tp/";
+        try {
+            new LinesCounter(dir).run();
+        } catch (InterruptedException | ExecutionException e) {
+            e.printStackTrace();
+        }
+    }
 }
--- src/main/java/ar/edu/itba/pod/j8/tp/tp2anexo/ej2_line_counter/LinesCounter.java
+++ src/main/java/ar/edu/itba/pod/j8/tp/tp2anexo/ej2_line_counter/LinesCounter.java
@@ -6,9 +6,7 @@ import java.nio.file.Path;
 import java.nio.file.Paths;
 import java.util.ArrayList;
 import java.util.List;
-import java.util.concurrent.CompletableFuture;
-import java.util.concurrent.ExecutorService;
-import java.util.concurrent.Executors;
+import java.util.concurrent.*;
 import java.util.function.Supplier;
 import java.util.stream.Collectors;
 import java.util.stream.Stream;
@@ -16,26 +14,30 @@ import java.util.stream.Stream;
 public class LinesCounter {
 
     private final Path path;
-    private long lines;
 
     public LinesCounter(String directory) {
         this.path = Paths.get(directory);
     }
 
-    public long run() {
+    public void run() throws InterruptedException, ExecutionException {
         final List<Path> paths = getFiles();
         final ExecutorService executor = Executors.newCachedThreadPool();
         final StringBuffer response = new StringBuffer(); // StringBuffer is thread-safe
         final List<CompletableFuture<String>> tasks = new ArrayList<>();
 
         paths.forEach(p -> {
-            CompletableFuture<Long> linesTask = CompletableFuture.supplyAsync()
+            CompletableFuture<Long> linesTask = CompletableFuture.supplyAsync(new FileLinesCounter(p), executor);
+            CompletableFuture<Long> sizeTask = CompletableFuture.supplyAsync(new FileSize(p), executor);
+            CompletableFuture<String> combined = linesTask.thenCombineAsync(sizeTask, (lines, size) -> String.format("%s: %d lines, %d bytes", p, lines, size));
+            combined.whenComplete((r, e) -> response.append(r).append("\n"));
+            tasks.add(combined);
         });
 
-
-        CompletableFuture countLines = ;
-        CompletableFuture fileSize = ;
-        CompletableFuture fileLinesCounter = ;
+        CompletableFuture[] taskArray = tasks.toArray(new CompletableFuture[0]);
+        CompletableFuture.allOf(taskArray).get();
+        executor.shutdown();
+        executor.awaitTermination(10, TimeUnit.SECONDS);
+        System.out.println(response);
     }
 
     private List<Path> getFiles() {
@@ -46,21 +48,59 @@ public class LinesCounter {
         }
         return new ArrayList<>();
     }
-}
 
-public class FileLinesCounter implements Supplier<Long> {
-    private final Path path;
 
-    public FileLinesCounter(Path path) {
-        this.path = path;
+    /**
+     * This class is used to count the lines of a file.
+     */
+    public static class FileLinesCounter implements Supplier<Long> {
+        private final Path path;
+
+        public FileLinesCounter(Path path) {
+            this.path = path;
+        }
+
+        @Override
+        public Long get() {
+            if (!Files.isRegularFile(path)) {
+                return 0L;
+            }
+
+            long lines = 0;
+            try (Stream<String> stream = Files.lines(path)) {
+                lines = stream.count();
+            } catch (Exception e) {
+                e.printStackTrace();
+            }
+
+            return lines;
+        }
     }
 
-    @Override
-    public Long get() {
-        if ()
+
+    /**
+     * This class is used to get the size of a file.
+     */
+    public static class FileSize implements Supplier<Long> {
+        private final Path path;
+
+        public FileSize(Path path) {
+            this.path = path;
+        }
+
+        @Override
+        public Long get() {
+            if (!Files.isRegularFile(path)) {
+                return 0L;
+            }
+
+            long size = 0;
+            try {
+                size = Files.size(path);
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+            return size;
+        }
     }
-}
-
-public class FileSize implements Supplier<Long> {
-
 }
